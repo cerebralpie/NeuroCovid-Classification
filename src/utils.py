b6bc0ee5eb295a2ddb8ca705455459cd8dfa585b
Leonardo Monteiro de Sousa <leonardo_04monteiro@hotmail.com>
2024-01-31 10:39:17 -0300

    Add function "load_data"

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,10 @@ File's docstring - To be done
 import tensorflow as tf
 import pydicom
 from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+
+SEED = 42
 
 
 def configure_gpu():
@@ -46,3 +50,24 @@ def dicom_to_tensor(dicom_file_path: Path) -> tf.Tensor:
     tensor = tf.convert_to_tensor(image_array, dtype=tf.float32)
 
     return tensor
+
+
+def load_data(root_dir: Path, split=0.1):
+    images = sorted(root_dir.glob("images/*"))
+    masks = sorted(root_dir.glob("masks/*"))
+
+    total_size = len(images)
+    valid_size = int(total_size * split)
+    test_size = int(total_size * split)
+
+    x_train, x_valid = train_test_split(images, test_size=valid_size,
+                                        random_state=SEED)
+    y_train, y_valid = train_test_split(masks, test_size=valid_size,
+                                        random_state=SEED)
+
+    x_train, x_test = train_test_split(x_train, test_size=test_size,
+                                       random_state=SEED)
+    y_train, y_test = train_test_split(y_train, test_size=test_size,
+                                       random_state=SEED)
+
+    return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
