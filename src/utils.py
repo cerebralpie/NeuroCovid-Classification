9098ad50bbec8cfbd636f314d16d27c4dfe8893a
Leonardo Monteiro de Sousa <leonardo_04monteiro@hotmail.com>
2024-02-05 17:08:25 -0300

    Update function configure_gpu

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 SEED = 42
 
 
-def configure_gpu():
+def configure_gpu() -> tf.distribute.Strategy:
     """
     Configures the GPU settings for TensorFlow if a GPU is available.
 
@@ -24,26 +24,25 @@ def configure_gpu():
     If no GPU is found, it gets the default distribution strategy and prints
     the number of replicas in sync.
 
+    Returns:
+        A TensorFlow device strategy
+
     Raises:
         RuntimeError: If there is an error while setting the GPU settings.
-
-    Returns:
-        None
     """
     gpus = tf.config.experimental.list_physical_devices('GPU')
 
     if gpus:
         try:
             for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+                tf.config.set_memory_growth(gpu, True)
+            tf.config.set_visible_devices(gpus[0], 'GPU')
             strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
             print("\nGPU Found! Using GPU...")
-        except RuntimeError as e:
-            print(e)
+        except RuntimeError:
+            raise
     else:
         strategy = tf.distribute.get_strategy()
-        print("\nNumber of replicas: ", strategy.num_replicas_in_sync)
 
     return strategy
 
